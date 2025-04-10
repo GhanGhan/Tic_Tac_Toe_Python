ec3a90486d507d5dbf5009a6ae3281aef62a2084
GhanGhan <ghanlawal@gmail.com>
2025-04-09 23:30:59 -0400

    Start planning program structure of Tic-Tac-Toe game

--- Console_Version/Tic-Tac-Toe.py
+++ Console_Version/Tic-Tac-Toe.py
@@ -1,3 +1,5 @@
+import gamePlay
+
 # Welcome Both players to the game
 print("\nWelcome to Tic-Tac-Toe!!!!!! \nPlayer 1 is \"X\", Player 2 is \"O\"! ")
 print("Type \"G\" then press \"Enter\" when you are ready to play!")
@@ -10,4 +12,5 @@ while key != "G":
         print("Invalid input, please enter the character \"G\"")
 
 print("Alright, lets start the game!!!")
+gamePlay.showGrid()
 
