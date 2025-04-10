c4f86c8477be633d1ab885115f4388a2ac561537
GhanGhan <ghanlawal@gmail.com>
2025-04-09 21:54:33 -0400

    Started console version of tic-tac-toe

---
+++ Console_Version/Tic-Tac-Toe.py
@@ -0,0 +1,13 @@
+# Welcome Both players to the game
+print("\nWelcome to Tic-Tac-Toe!!!!!! \nPlayer 1 is \"X\", Player 2 is \"O\"! ")
+print("Type \"G\" then press \"Enter\" when you are ready to play!")
+
+key = ""
+
+while key != "G":
+    key = input(": ")
+    if key != "G":
+        print("Invalid input, please enter the character \"G\"")
+
+print("Alright, lets start the game!!!")
+
